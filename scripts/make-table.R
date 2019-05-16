4573cd744dc05784b801cbeb56c7842184b7b0ab
hettling <hannes.hettling@gmail.com>
2019-05-16 11:09:45 +0200

    new data

--- scripts/make-table.R
+++ scripts/make-table.R
@@ -1,10 +1,10 @@
-tab1 <- read.csv('../data/hannes-ttik-1.csv', header=T)
-tab2 <- read.csv('../data/hannes-ttik-2.csv', header=T)
+tab1 <- read.csv('../data/hannes-ttik-1.csv', header=T, stringsAsFactors=F)
+tab2 <- read.csv('../data/hannes-ttik-2.csv', header=T, stringsAsFactors=F)
 tab3 <- read.csv('../data/hannes-ttik-3.csv', header=T)
 
 tab <- rbind(tab1, tab2, tab3)
 
-moeder <- data.frame(read.csv('../data/20190429-museum-objects.tsv', header=T, sep="\t", skip=1))
+##moeder <- data.frame(read.csv('../data/20190429-museum-objects.tsv', header=T, sep="\t", skip=1))
 
 ##for (c in colnames(tab)) {
 ##    moeder[,c] <- ""
@@ -22,8 +22,12 @@ moeder <- data.frame(read.csv('../data/20190429-museum-objects.tsv', header=T, s
 
 ##write.table(moeder, row.names=F, file="../data/20190429-museum-objects-images.tsv", sep="\t", quote=F)
 
-missing <- moeder$Registrationnumber[which(! moeder$Registrationnumber %in% tab$REGISTRATIONNUMBER)]
+#missing <- moeder$Registrationnumber[which(! moeder$Registrationnumber %in% tab$REGISTRATIONNUMBER)]
 
-for (i in seq_along(missing)) {
-    tab[i, 'REGISTRATIONNUMBER'] <- missing[i]
-}
+#for (i in seq_along(missing)) {
+#    tab[i, 'REGISTRATIONNUMBER'] <- missing[i]
+                                        #}
+
+tab <- tab[order(trimws(tab$GENUS), trimws(tab$EPITHET), trimws(tab$PRESERVEDPART), trimws(tab$REGISTRATIONNUMBER),  trimws(tab$URL)), ]
+tab <- tab[, c("REGISTRATIONNUMBER", "INST_COLL_SUBCOLL", "TRANSFERREASON", "FULLSCIENTIFICNAME", "GENUS", "EPITHET", "PRESERVEDPART", "ICOONURL", "URL")]
+write.table(tab, row.names=F, file="../test.tsv", sep="\t")
