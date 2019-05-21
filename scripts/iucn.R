b92b56dfd23a875caa96c2d6d4a26645e9d69373
hettling <hannes.hettling@gmail.com>
2019-05-21 18:20:22 +0200

    nba lookup

--- scripts/iucn.R
+++ scripts/iucn.R
@@ -1,19 +1,46 @@
 require('rredlist')
+require('nbaR')
 
-data <- read.csv('../data/20190510-museum-objects.tsv', header=T, sep="\t", skip=1)
+data <- read.csv('../data/20190510-2-museum-objects.tsv', header=T, sep="\t", skip=1)
+
+## sc <- SpecimenClient$new()
 
 
-get_iucn_data <- function(name) {
+get_iucn_data <- function(row) {
+    name <- row["SCname"]
     cat("Querying IUCN for ", name, "\n")
+    ## parse out characters that make icun api error
+    name <- gsub("/", "", name)
     result <- c(iucn_id=NA, iucn_category=NA)
     if (name != "tba" & name != "") {
         s <- rl_search(name)
-        if (length(s$result > 0)) {
+        if (length(s$result > 0)) {          
             result['iucn_id'] <- s$result$taxonid
             result['iucn_category'] <- s$result$category
+            cat("\tFound IUCN data\n")
+        } else {
+            regnr <- row["Registratienummer"]
+            cat("\tIUCN query gives empty result, checking NBA for regnr ", regnr, "\n")
+            exists <- specimen_exists(regnr)
+
+            if (exists) {                
+                cat("\tRegnr exists, getting name from identifications\n")
+                tab <- specimen_find_by_unit_id(regnr)
+                cat("\tFound ", length(tab$identifications), " identifications\n)")
+                for(i in seq_along(tab$identifications)) {
+                    id <- tab$identifications[[i]]
+                    new_name <- paste(id$defaultClassification$genus, id$defaultClassification$specificEpithet)
+                    cat("\tQuerying IUCN with new name ", new_name, "\n")
+                    s <- rl_search(new_name)
+                    if (length(s$result > 0)) {          
+                        result['iucn_id'] <- s$result$taxonid
+                        result['iucn_category'] <- s$result$category
+                    }
+                }
+            }           
         }
     }    
     return (result)
 }
 
-iucn_data <- do.call(rbind, lapply(as.character(data$SCname), get_iucn_data))
+iucn_data <- do.call(rbind, apply(data, 1, get_iucn_data))
