dbcb8be02b424bb1515a5103af60c1effa23426d
hettling <hannes.hettling@gmail.com>
2019-07-08 17:15:09 +0200

    more ids now!

--- scripts/make-crs-queries.R
+++ scripts/make-crs-queries.R
@@ -1,19 +1,22 @@
 ## extract registrationnumbers
 ## from "moederlijst" for queries in CRS
 
-data <- read.csv('../data/20190429-museum-objects.tsv', header=T, sep="\t", skip=1)
-regnrs <- as.character(data$Registrationnumber)
+data <- read.csv('../data/20190607-museum-objects.tsv', header=T, sep="\t", skip=1)
+regnrs <- as.character(data$Registratienummer)
 regnrs <- regnrs[which(regnrs != "")]
 ## filter out the long ones (doesn't work for query)
 regnrs <- regnrs[-which(sapply(regnrs, nchar) > 20)]
 regnrs <- regnrs[-which(startsWith(regnrs, "tba"))]
 regnrs <- regnrs[-which(startsWith(regnrs, "leen"))]
 
+regnrs <- unique(regnrs)
+
 ##
-l <- split(regnrs, sort(rep_len(1:3, length(regnrs))))
+l <- split(regnrs, sort(rep_len(1:4, length(regnrs))))
 regnrstr_1 <- paste(paste0("\'", l[[1]], "\'"), collapse=",")
 regnrstr_2 <- paste(paste0("\'", l[[2]], "\'"), collapse=",")
 regnrstr_3 <- paste(paste0("\'", l[[3]], "\'"), collapse=",")
+regnrstr_4 <- paste(paste0("\'", l[[4]], "\'"), collapse=",")
 
 ## make query
 
@@ -41,3 +44,10 @@ query <- paste("SELECT s.xmlbeschrijvingid\n",
                paste("(", regnrstr_3, ")")
                )
 cat(query, file="../query-3.sql")
+
+query <- paste("SELECT s.xmlbeschrijvingid\n",
+               "FROM ncrs_specimen s", 
+               "where s.registrationnumber IN\n",
+               paste("(", regnrstr_4, ")")
+               )
+cat(query, file="../query-4.sql")
